6055d4b0e93391478d3d1efbc76724459c97587c
Jinseong <hdss8234@gmail.com>
2024-05-19 19:41:32 +0900

    refactor : Employee 도메인 구축에 따른 메서드 추가

--- src/main/java/com/group/commute/domain/team/TeamRepository.java
+++ src/main/java/com/group/commute/domain/team/TeamRepository.java
@@ -3,6 +3,10 @@ package com.group.commute.domain.team;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface TeamRepository extends JpaRepository<Team, Long> {
+
+    Optional<Team> findByName(String teamName);
 }
